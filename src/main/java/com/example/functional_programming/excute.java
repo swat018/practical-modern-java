3f96a497feb3ecb11341f1c5f32b820456ef75cc
jinwoo park <swat018@gmail.com>
2021-08-09 06:52:16 +0900

    함수형 프로그래밍 - 3.2 여행 상품 개발

--- src/main/java/com/example/functional_programming/excute.java
+++ src/main/java/com/example/functional_programming/excute.java
@@ -2,9 +2,17 @@ package com.example.functional_programming;
 
 import com.example.interface_class.Worker;
 
+import java.util.List;
+
 public class excute {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        SearchingTravel travelSearch = new SearchingTravel();
+        // 베트남 여행 상품 목록 조회
+        List<TravleInfo> searchList = travelSearch.searchTravelInfo(SearchingTravel.COUNTRY_VIETNAM);
+
+        for(TravleInfo searchTravel : searchList) {
+            System.out.println(searchTravel);
+        }
     }
 }
