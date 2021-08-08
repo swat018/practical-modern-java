678d5d5ef9548331952a4d460278ffa465ff3c95
jinwoo park <swat018@gmail.com>
2021-08-09 07:54:08 +0900

    함수형 프로그래밍 - 3.3 조회 조건 추가

---
+++ src/main/java/com/example/functional_programming/NewMethodSearchingTravel.java
@@ -0,0 +1,87 @@
+package com.example.functional_programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewMethodSearchingTravel {
+
+    public static final String COUNTRY_VIETNAM = "vietnam";
+    public static final String COUNTRY_PHILLIPHINE = "philliphine";
+    public static final String COUNTRY_TAILAND = "tailand";
+
+    // 여행 상품 저장
+    private List<TravleInfo> travleInfoList = new ArrayList<>();
+
+    public NewMethodSearchingTravel() {
+        initializeProduct();
+    }
+
+    // 데이터를 초기화하여 외부에서 호출하지 못하도록 private으로 선언
+    private void initializeProduct() {
+        TravleInfo cebu = new TravleInfo();
+        cebu.setName("Cebu Travel");
+        cebu.setCountry(COUNTRY_PHILLIPHINE);
+        cebu.setCity("cebu");
+        cebu.setDays(5);
+        cebu.setNights(3);
+        travleInfoList.add(cebu);
+
+        TravleInfo boracay = new TravleInfo();
+        boracay.setName("Boracay Travel");
+        boracay.setCountry(COUNTRY_PHILLIPHINE);
+        boracay.setCity("boracay");
+        boracay.setDays(5);
+        boracay.setNights(3);
+        travleInfoList.add(boracay);
+
+        TravleInfo hanoi = new TravleInfo();
+        hanoi.setName("Hanoi Travel");
+        hanoi.setCountry(COUNTRY_VIETNAM);
+        hanoi.setCity("hanoi");
+        hanoi.setDays(3);
+        hanoi.setNights(2);
+        travleInfoList.add(hanoi);
+
+        TravleInfo danang = new TravleInfo();
+        danang.setName("Danang Travel");
+        danang.setCountry(COUNTRY_VIETNAM);
+        danang.setCity("danang");
+        danang.setDays(6);
+        danang.setNights(4);
+        travleInfoList.add(danang);
+
+        TravleInfo bankok = new TravleInfo();
+        bankok.setName("Bankok Travel");
+        bankok.setCountry(COUNTRY_TAILAND);
+        bankok.setCity("bankok");
+        bankok.setDays(5);
+        bankok.setNights(3);
+        travleInfoList.add(bankok);
+    }
+
+    // 국가 정보에 기반해서 여행 상품을 조회한다.
+    public List<TravleInfo> searchTravelInfoByCountry(String country) {
+        List<TravleInfo> returnValue = new ArrayList<>();
+
+        for(TravleInfo travleInfo : travleInfoList) {
+            if(country.equals(travleInfo.getCountry())) {
+                returnValue.add(travleInfo);
+            }
+        }
+
+        return returnValue;
+    }
+
+    // 도시 정보에 기반해서 여행 상품을 조회한다.
+    public List<TravleInfo> searchTravelInfoByCity(String city) {
+        List<TravleInfo> returnValue = new ArrayList<>();
+
+        for(TravleInfo travleInfo : travleInfoList) {
+            if(city.equals(travleInfo.getCity())) {
+                returnValue.add(travleInfo);
+            }
+        }
+
+        return returnValue;
+    }
+}
--- src/main/java/com/example/functional_programming/excute.java
+++ src/main/java/com/example/functional_programming/excute.java
@@ -7,12 +7,28 @@ import java.util.List;
 public class excute {
 
     public static void main(String[] args) {
-        SearchingTravel travelSearch = new SearchingTravel();
-        // 베트남 여행 상품 목록 조회
-        List<TravleInfo> searchList = travelSearch.searchTravelInfo(SearchingTravel.COUNTRY_VIETNAM);
+//        SearchingTravel travelSearch = new SearchingTravel();
+//        // 베트남 여행 상품 목록 조회
+//        List<TravleInfo> searchList = travelSearch.searchTravelInfo(SearchingTravel.COUNTRY_VIETNAM);
+//
+//        for(TravleInfo searchTravel : searchList) {
+//            System.out.println(searchTravel);
+//        }
 
-        for(TravleInfo searchTravel : searchList) {
+        NewMethodSearchingTravel travelSearch = new NewMethodSearchingTravel();
+        // 국가명을 기준으로 조회하는 예제
+        List<TravleInfo> searchListByCountry = travelSearch.searchTravelInfoByCountry("vietnam");
+
+        for(TravleInfo searchTravel : searchListByCountry) {
             System.out.println(searchTravel);
         }
+
+        // 도시명을 기준으로 조회하는 예제
+        List<TravleInfo> searchListByCity = travelSearch.searchTravelInfoByCity("cebu");
+
+        for(TravleInfo searchTravel : searchListByCity) {
+            System.out.println(searchTravel);
+        }
+
     }
 }
