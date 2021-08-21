b0cc9e3d71b0c7407b498f22b9f6b69d40e84050
jinwoo park <swat018@gmail.com>
2021-08-21 11:30:58 +0900

    람다와 함수형 인터페이스 - 람다 표현식으로 전환

--- src/main/java/com/example/lambda/ThreadExample.java
+++ src/main/java/com/example/lambda/ThreadExample.java
@@ -3,14 +3,20 @@ package com.example.lambda;
 public class ThreadExample {
 
     public static void main(String[] args) {
-        // 스레드를 생성한다.
-        Thread thread = new Thread(new Runnable() {
-            // run 메서드를 구현한다.
-            @Override
-            public void run() {
-                System.out.println("Hello World");
-            }
-        });
+//        // 스레드를 생성한다.
+//        Thread thread = new Thread(new Runnable() {
+//            // run 메서드를 구현한다.
+//            @Override
+//            public void run() {
+//                System.out.println("Hello World");
+//            }
+//        });
+//
+//        thread.start();
+//    }
+
+        Thread thread = new Thread( () -> System.out.println("Hello World") );
+
 
         thread.start();
     }
