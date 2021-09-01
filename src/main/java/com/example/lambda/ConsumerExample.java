369af0d958fdc981e4a2369204d6cace1f4f5616
jinwoo park <swat018@gmail.com>
2021-09-01 17:24:24 +0900

    람다와 함수형 인터페이스 - 함수형 인터페이스(Consumer 인터페이스)

---
+++ src/main/java/com/example/lambda/ConsumerExample.java
@@ -0,0 +1,24 @@
+package com.example.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void executeConsumer(List<String> nameList, Consumer<String> consumer) {
+        for(String name : nameList) {
+            // 메서드의 두 번째 인수로 전달된 람다 표현식을 실행시킨다.
+            consumer.accept(name);
+        }
+    }
+
+    //Consumer 인터페이스 활용 예쩨
+    public static void main(String[] args) {
+        List<String> nameList = new ArrayList<>();
+        nameList.add("정수빈");
+        nameList.add("김재호");
+        nameList.add("오재원");
+        nameList.add("이영하");
+        ConsumerExample.executeConsumer(nameList, (String name) -> System.out.println(name));
+    }
+}
