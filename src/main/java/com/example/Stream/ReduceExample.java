5b52e396a1ef8f15457ebde4d3958e288e4432b1
jinwoo park <swat018@gmail.com>
2021-09-10 18:33:00 +0900

    스트림 API - 리듀스 연산 (합계를 구하는 방법들 - 스트림의 collect 연산을 이용)

--- src/main/java/com/example/Stream/ReduceExample.java
+++ src/main/java/com/example/Stream/ReduceExample.java
@@ -2,12 +2,18 @@ package com.example.Stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReduceExample {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
+        // IntStream 인터페이스 이용
         int intStreamSum = intList.stream().mapToInt(Integer::intValue).sum();
         System.out.printf("IntStream 이용 sum : %s \n", intStreamSum);
+
+        // 스트림의 collect 연산을 이용
+        int sum2 = intList.stream().mapToInt(Integer::intValue).sum();
+        System.out.printf("Stream.collect 이용 sum : %s \n", sum2);
     }
 }
