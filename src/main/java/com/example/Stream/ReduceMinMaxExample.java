be42d9ddaf5b787ca71e23f52878062257b76182
jinwoo park <swat018@gmail.com>
2021-09-11 05:36:48 +0900

    스트림 API - 리듀스 연산 (리듀스 연산 응용)

---
+++ src/main/java/com/example/Stream/ReduceMinMaxExample.java
@@ -0,0 +1,20 @@
+package com.example.Stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReduceMinMaxExample {
+
+    // reduce 메서드를 이용하여 최대값/최소값 구하는 예
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(4, 2, 8, 1, 9 ,6, 7, 3, 5);
+
+        // 최대값 구하기
+        int max = intList.stream().reduce(0, Integer::max);
+        System.out.printf("최대값 : %s\n", max);
+
+        // 최소값 구하기
+        int min = intList.stream().reduce(0, Integer::min);
+        System.out.printf("최소값 : %s\n", min);
+    }
+}
