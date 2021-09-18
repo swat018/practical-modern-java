95f8f2eff42d4e18548ebdb78e5674d393188ad1
jinwoo park <swat018@gmail.com>
2021-09-18 18:04:10 +0900

    병렬 프로그래밍 - 컨커런트 API(Future와 CompletableFuture)

--- src/main/java/com/example/ConcurrentAPI/InsuranceCalculator.java
+++ src/main/java/com/example/ConcurrentAPI/InsuranceCalculator.java
@@ -19,14 +19,30 @@ public class InsuranceCalculator {
         return price;
     }
 
+    // 비동기 처리하는 API
+    public Future<Integer> calculatePriceAsync(Map condition) {
+        CompletableFuture<Integer> future = new CompletableFuture<>();
+        // 스레드를 생성하고 실행할 작업을 CompletableFuture에 등록한다.
+        new Thread( () -> {
+            int price = calculatePrice(condition);
+            // 처리 상태에 대한 레퍼런스를 등록한다.
+            future.complete(price);
+        }).start();
+        return future;
+    }
+
     public static void main(String[] args) {
-//        InsuranceCalculator cal = new InsuranceCalculator();
-//
-//        // 5회 걸쳐 계산한다.
-//        for(int i=0; i<5; i++) {
-//            System.out.printf("계산 차수 %s : %s\n", (i+1), cal.calculatePrice(null));
-//        }
-        // 1. 비동기 방식으로 전환
+        InsuranceCalculator cal = new InsuranceCalculator();
+
+        // 동기 방식 처리
+        System.out.println("동기 방식 호출");
+        // 5회 걸쳐 계산한다.
+        for(int i=0; i<5; i++) {
+            System.out.printf("계산 차수 %s : %s\n", (i+1), cal.calculatePrice(null));
+        }
+
+        // 비동기 방식 처리 1
+        System.out.println("비동기 방식 호출 1");
         ExecutorService service = Executors.newFixedThreadPool(5);
         List<Future<Integer>> futureList = new ArrayList<>();;
 
@@ -45,6 +61,24 @@ public class InsuranceCalculator {
             } catch(InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
+        });
+
+        // 비동기 방식 처리 2
+        System.out.println("비동기 방식 호출 2");
+
+        futureList.clear();
+
+        for(int i=0; i<5; i++) {
+            Future<Integer> future = cal.calculatePriceAsync(null);
+            futureList.add(future);
+        }
+
+        futureList.forEach((future) -> {
+           try {
+               System.out.printf("계산 결과 : %s\n", future.get());
+           } catch (InterruptedException | ExecutionException e) {
+               e.printStackTrace();
+           }
         });
     }
 }
