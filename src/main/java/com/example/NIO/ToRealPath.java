09bec0cd9b1ef26490d1f29050bd7c5914b820d5
jinwoo park <swat018@gmail.com>
2021-09-26 18:40:06 +0900

    파일I/O(NIO2.0) - Path를 이용한 경로 관리(Path 정보 활용)

---
+++ src/main/java/com/example/NIO/ToRealPath.java
@@ -0,0 +1,34 @@
+package com.example.NIO;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ToRealPath {
+    // toRealPath 사용 예
+    public static void main(String[] args) {
+        try {
+            // 심볼릭 링크를 실제 경로로 전환해서 객체 생성
+            Path path = Paths.get("/Users/jinwoopark/git").toRealPath();
+            print(path);
+
+            // 심볼릭 링크 경로로 객체 생성
+            path = Paths.get("/Users/jinwoopark/git");
+            print(path);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Path 객체의 정보를 출력한다.
+    private static void print(Path path) {
+        System.out.format("toString: %s\n", path.toString());
+        System.out.format("getFileName: %s\n", path.getFileName());
+        System.out.format("getName(0): %s\n", path.getName(0));
+        System.out.format("getNameCount: %s\n", path.getNameCount());
+        System.out.format("subpath(0,2): %s\n", path.subpath(0,2));
+        System.out.format("getParent: %s\n", path.getParent());
+        System.out.format("getRoot: %s\n", path.getRoot());
+    }
+}
