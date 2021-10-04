ceac6f02acb9a555493845760d6739f89d9ce3b9
jinwoo park <swat018@gmail.com>
2021-10-04 10:32:37 +0900

    파일I/O(NIO2.0) - 파일 생성,쓰기,읽기 (스트림 I/O)

--- src/main/java/com/example/NIO/StreamFileIO.java
+++ src/main/java/com/example/NIO/StreamFileIO.java
@@ -19,24 +19,44 @@ public class StreamFileIO {
         Path targetPath = Paths.get(args[1]);
 
         // InputStream, OutputStream을 이용해서 파일을 처리한다.
+//        try (InputStream input = Files.newInputStream(sourcePath);
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+//                OutputStream out = Files.newOutputStream(targetPath);
+//                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
+//            String line = null;
+//
+//            // 파일에서 데이터를 읽어들인다. 한 줄씩 읽어들이는 예제이다.
+//            while((line = reader.readLine()) != null) {
+//                System.out.println(line);
+//
+//                // 특정한 비즈니스 로직을 구현한다.
+//                // ...
+//
+//                // 데이터를 파일에 저장한다.
+//                writer.write(line, 0, line.length());
+//            }
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//        }
+
+        // InputStream, OutputStream 기반 처리
         try (InputStream input = Files.newInputStream(sourcePath);
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                BufferedInputStream binput = new BufferedInputStream(input);
                 OutputStream out = Files.newOutputStream(targetPath);
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
-            String line = null;
+                BufferedOutputStream bout = new BufferedOutputStream(out);) {
 
-            // 파일에서 데이터를 읽어들인다. 한 줄씩 읽어들이는 예제이다.
-            while((line = reader.readLine()) != null) {
-                System.out.println(line);
+            int index = 0;
+            byte[] buffer = new byte[1024];
 
-                // 특정한 비즈니스 로직을 구현한다.
-                // ...
-
-                // 데이터를 파일에 저장한다.
-                writer.write(line, 0, line.length());
+            // 파일에서 데이터를 읽어 들인다.
+            // 한 줄씩 읽지 않고 일정 크기의 버퍼만큼 읽어 들인다.
+            while ((index = binput.read(buffer)) != -1) {
+                bout.write(buffer, 0, index);
             }
+
         } catch(IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            System.err.format("IOException: %s%n", e);
         }
 
     }
