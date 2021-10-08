cc34029292aba8d3fd058e796c971cda49465a02
jinwoo park <swat018@gmail.com>
2021-10-08 23:50:19 +0900

    파일I/O(NIO2.0) - 디렉터리 처리(목록 필터링)

--- src/main/java/com/example/NIO/OldFileFilter.java
+++ src/main/java/com/example/NIO/OldFileFilter.java
@@ -11,13 +11,20 @@ public class OldFileFilter {
         File file = new File("/Users/jinwoopark/Desktop/git/practical-modern-java");
         // FilenameFilter 인터페이스를 구현해서 필터링한다.
         // 람다 표현식으로 변경할 수 있다.
-        String[] filteredFile = file.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                System.out.println(dir + File.separator + name);
-                return name.toLowerCase().endsWith(".md");
-            }
-        });
+//        String[] filteredFile = file.list(new FilenameFilter() {
+//            @Override
+//            public boolean accept(File dir, String name) {
+//                System.out.println(dir + File.separator + name);
+//                return name.toLowerCase().endsWith(".md");
+//            }
+//        });
+//
+//        for (String fileName : filteredFile) {
+//            System.out.println(fileName);
+//        }
+
+        // 람다 표현식을 이용한 FilenameFilter 구현
+        String[] filteredFile = file.list((File dir, String name) -> name.toLowerCase().endsWith(".md"));
 
         for (String fileName : filteredFile) {
             System.out.println(fileName);
