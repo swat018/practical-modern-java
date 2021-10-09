f75fc90e6c3b22de84435ae985207a7d4b4fa57e
jinwoo park <swat018@gmail.com>
2021-10-09 16:42:33 +0900

    파일I/O(NIO2.0) - 디렉터리 처리(목록 필터링)

---
+++ src/main/java/com/example/NIO/NewFileFilter2.java
@@ -0,0 +1,30 @@
+package com.example.NIO;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+/**
+ *  DirectoryStream.Filter를 이용한 파일 필터링 예
+ */
+public class NewFileFilter2 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("/Users/jinwoopark/Desktop/git/practical-modern-java");
+
+        // 필터를 정의한다.
+        DirectoryStream.Filter<Path> filter = new DirectoryStream.Filter<Path>() {
+            @Override
+            public boolean accept(Path file) throws IOException {
+                return (Files.isDirectory(file));
+            }
+        };
+
+        // DirectoryStream.Filter를 기준으로 목록을 조회한다.
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, filter)) {
+            for (Path file : stream) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            System.err.println(e);
+        }
+    }
+}
