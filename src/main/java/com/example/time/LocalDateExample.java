3718713b3faebc65bbff9d0f1da557b464734298
jinwoo park <swat018@gmail.com>
2021-10-12 23:58:23 +0900

    날짜와 시간 - 날짜

---
+++ src/main/java/com/example/time/LocalDateExample.java
@@ -0,0 +1,17 @@
+package com.example.time;
+
+import java.time.LocalDate;
+
+/**
+ * LocalDate 클래스 사용 예제
+ * @author jinwoopark
+ */
+public class LocalDateExample {
+
+    public static void main(String[] args) {
+        // 현재 날짜 객체를 생성
+        LocalDate today = LocalDate.now();
+        // 과거 날짜 객체를 생성
+
+    }
+}
