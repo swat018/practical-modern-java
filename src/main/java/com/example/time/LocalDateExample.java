745bee8f902e4b97e82d115711b7c663573e3399
jinwoo park <swat018@gmail.com>
2021-10-14 21:52:57 +0900

    날짜와 시간 - 날짜

--- src/main/java/com/example/time/LocalDateExample.java
+++ src/main/java/com/example/time/LocalDateExample.java
@@ -1,6 +1,7 @@
 package com.example.time;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 /**
  * LocalDate 클래스 사용 예제
@@ -12,6 +13,13 @@ public class LocalDateExample {
         // 현재 날짜 객체를 생성
         LocalDate today = LocalDate.now();
         // 과거 날짜 객체를 생성
+        LocalDate birthday = LocalDate.of(2019, Month.JANUARY, 1);
+        // 과거 날짜 객체 정보를 수정
+        LocalDate nextBDay = birthday.withYear(today.getYear());
 
+        // 날짜 객체 비교, 생일이 지났는지 여부를 판단
+        if (nextBDay.isBefore(today) || nextBDay.isEqual(today)) {
+            nextBDay = nextBDay.plusYears(1);
+        }
     }
 }
