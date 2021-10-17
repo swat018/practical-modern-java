502dd32035af6ff8cd0afea02ae7c3493d892673
jinwoo park <swat018@gmail.com>
2021-10-18 08:46:34 +0900

    날짜와 시간 - 타임존과 오프셋

---
+++ src/main/java/com/example/time/ListTimeZone.java
@@ -0,0 +1,12 @@
+package com.example.time;
+
+/**
+ * 현재 타임존 기반으로 시차가 존재하는 타임존 목록을 구한다.
+ * Asia/Seoul과 Asia/Japan은 시차가 없기 때문에 출력되지 않는다.
+ * @author jinwoopark
+ */
+public class ListTimeZone {
+    public static void main(String[] args) {
+
+    }
+}
