b6280898b90aa327c5256c6281664339dad26cb0
jinwoo park <swat018@gmail.com>
2021-11-22 23:59:19 +0900

    유용한 새 기능들 - try문의 개선(try with resource)

---
+++ src/main/java/com/example/exception/Java9FileReader.java
@@ -0,0 +1,30 @@
+package com.example.exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Java 9 이상이어야 컴파일된다.
+ * @author jinwoopark
+ */
+public class Java9FileReader {
+    private static final String FILENAME = "/Users/jinwoopark/Desktop/git/practical-modern-java/test/filename.txt";
+
+    public static void main(String[] args) {
+        // 외부에쇼ㅓ 생성하였다.
+        BufferedReader br = new BufferedReader(new FileReader(FILENAME));
+
+        // resource 구문 안에 변수를 지정하였다.
+        try(br) {
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
